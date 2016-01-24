1da9212295d83d4be9ba70d2b0d5a2482ff0b35c
Sam Corcos <sam@corcos.io>
2016-01-24 17:23:19 -0500

    componentize comment, profile styling

---
+++ frontend/app/components/Post/comment.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+import CSSModules from 'react-css-modules'
+import styles from './comment.css'
+
+/* How to use Comment component:
+
+  <CommentComponent
+    key={id} // this is not strictly necessary
+    upvotes={upvotes}
+    author={author} // must username, profile (image), and points
+    body={body}
+  />
+
+*/
+
+const CommentComponent = React.createClass({
+  render() {
+    return (
+      <div>
+        <div className={ styles.row }>
+          <div className={ styles.voting }>
+            <div className={ styles.arrowUp }></div>
+            <div className={ styles.votes }>{ this.props.upvotes }</div>
+            <div className={ styles.arrowDown }></div>
+          </div>
+          <div className={ styles.profileImage }>
+            <div className={ styles.profilePlaceholder }></div>
+          </div>
+          <div className={ styles.profileDetails }>
+            <div>
+              <h3>{ this.props.author.username }</h3>
+            </div>
+            <div className={ styles.pointsBadge }>
+              <span> { this.props.author.points } </span>
+              <span> Badge </span>
+            </div>
+          </div>
+          <div className={ styles.reply }>
+            <a href="#">reply</a>
+          </div>
+        </div>
+        <div className={ styles.row }>
+          <p className={ styles.commentBody }>
+            { this.props.body }
+          </p>
+        </div>
+      </div>
+    )
+  }
+})
+
+export default CSSModules(CommentComponent, styles)
--- frontend/app/components/Profile/userfeed.js
+++ frontend/app/components/Profile/userfeed.js
@@ -5,21 +5,92 @@ import CSSModules from 'react-css-modules'
 import styles from './styles.css'
 
 import PostComponent from 'components/Home/post'
+import CommentComponent from 'components/Post/comment'
 
-let posts = [
-  { thumbnail: "url", title: "This is the title of the first post.", votes: 29},
-  { thumbnail: "url", title: "Files are interesting", votes: 42},
-  { thumbnail: "url", title: "I wonder where the images will be stored", votes: 54},
-  { thumbnail: "url", title: "Four posts should be enough", votes: 2}
+let postsData = [
+  {
+    upvotes: Math.floor(Math.random() * 100),
+    thumbnail: "url",
+    title: "This is the title of the post!",
+    author: {
+      username: "Bobby Tables",
+      profile: "url"
+    },
+    timestamp: (new Date).getTime(),
+    comments: Math.floor(Math.random() * 10),
+    id: "1"
+  },
+  {
+    upvotes: Math.floor(Math.random() * 100),
+    thumbnail: "url",
+    title: "This is a really long title for a post. How long do we want to allow? We should limit this.",
+    author: {
+      username: "Bobby Tables",
+      profile: "url"
+    },
+    timestamp: (new Date).getTime(),
+    comments: Math.floor(Math.random() * 10),
+    id: "2"
+  },
+  {
+    upvotes: Math.floor(Math.random() * 100),
+    thumbnail: "url",
+    title: "This is the title of the post! this is way too long. i want to see what happens when i write something that goes into the third line",
+    author: {
+      username: "Bobby Tables",
+      profile: "url"
+    },
+    timestamp: (new Date).getTime(),
+    comments: Math.floor(Math.random() * 10),
+    id: "3"
+  }
 ]
 
-let comments = [
+let postList = postsData.map(function(post) {
+  return (
+    <PostComponent
+      key={post.id} // this is not strictly necessary
+      upvotes={post.upvotes}
+      thumbnail={post.thumbnail}
+      title={post.title}
+      author={post.author} // must have "username" and "profile" for image
+      timestamp={post.timestamp}
+      comments={post.comments} // this is the comment count
+    />
+  )
+})
 
-]
 
-let upvotes = [
+let commentData = [{
+  author: {
+    profile: "url",
+    username: "Bobby Tables",
+    points: Math.floor(Math.random() * 1000),
+    badge: "url"
+  },
+  upvotes: Math.floor(Math.random() * 100),
+  timestamp: (new Date).getTime(),
+  body: "This is the body of the post. Not every post will have stuff written here. It's entirely up to the author. Sometimes people might want to go on a long rant here and maybe explain some of what is going on in the link they provided. There are plenty of reasons to want to add content here.",
+  id: "1"
+}]
 
-]
+let commentList = commentData.map(function(comment) {
+  return (
+    <CommentComponent
+      key={comment.id}
+      upvotes={comment.upvotes}
+      author={comment.author}
+      body={comment.body}
+    />
+  )
+})
+
+let upvotesData = []
+
+let upvoteList;
+if (upvotesData.length === 0) {
+  upvoteList = <div className={ styles.noUpvotes }>No upvotes yet!</div>
+}
 
 const UserFeed = React.createClass({
   // TODO move this from local state
@@ -47,7 +118,11 @@ const UserFeed = React.createClass({
             onClick={this.handleChange.bind(null, "upvotes")}
             className={ this.state.feed === "upvotes" ? styles.feedTitleActive : styles.feedTitle }>Upvotes</h2>
         </div>
-
+        <div className={ styles.feedList }>
+          { this.state.feed === "posts" ? postList : null }
+          { this.state.feed === "comments" ? commentList : null }
+          { this.state.feed === "upvotes" ? upvoteList : null}
+        </div>
       </section>
     )
   }
